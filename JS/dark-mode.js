fe051df973c04f8b89f579ff2e27857840ca9603
GabrielKobielski <kobielski2002@gmail.com>
2025-06-23 23:54:11 -0300

    recap

--- JS/dark-mode.js
+++ JS/dark-mode.js
@@ -1,9 +1,10 @@
 let trilho = document.getElementById('trilho')
 let body = document.querySelector('body')
 
-trilho.addEventListener('click',()=>{
+trilho.addEventListener('click', () => {
   trilho.classList.toggle('light')
   body.classList.toggle('light')
 })
 
 //<script src="js/start.js"></script>
+
