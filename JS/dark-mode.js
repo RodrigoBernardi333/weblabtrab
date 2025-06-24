dd42e1ef8b11de469656a4d46df99b786612ac15
GabrielKobielski <kobielski2002@gmail.com>
2025-06-24 03:12:00 -0300

    atualização da main

--- JS/dark-mode.js
+++ JS/dark-mode.js
@@ -8,9 +8,18 @@ trilho.addEventListener('click', () => {
 
 //<script src="js/start.js"></script>
 
- document.addEventListener("DOMContentLoaded", function () {
-    const texto = document.getElementById('text4');
+document.addEventListener("DOMContentLoaded", function () {
+  const texto = document.getElementById('text4');
 
-    texto.addEventListener('click', () => {
-      texto.classList.toggle('piscando');
-})});
+  texto.addEventListener('click', () => {
+    texto.classList.toggle('piscando');
+  })
+});
+const slideContainer = document.getElementById('slide-container');
+const totalSlides = slideContainer.children.length;
+let currentIndex = 0;
+
+setInterval(() => {
+  currentIndex = (currentIndex + 1) % totalSlides;
+  slideContainer.style.transform = `translateX(-${currentIndex * 300}px)`;
+}, 1000); // Troca a 
