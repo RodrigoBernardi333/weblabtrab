875cf59484709b4ceb461fbe21ca86f1f6d60e1e
GabrielKobielski <kobielski2002@gmail.com>
2025-06-25 21:55:14 -0300

    feat: Update main

--- JS/dark-mode.js
+++ JS/dark-mode.js
@@ -1,28 +1,9 @@
 function toggleAcessibilidadeMenu() {
-    const menu = document.getElementById('acessibilidade-menu');
-    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
+  const menu = document.getElementById('acessibilidade-menu');
+  menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
 }
 
 function alterarFonte(fonte) {
-    document.body.style.fontFamily = fonte;
+  document.body.style.fontFamily = fonte;
 }
 
-
-var msgCookies = document.getElementById('cookies-msg');
-
-if (localStorage.getItem('lgpd') === 'sim') {
-  msgCookies.classList.remove('mostrar');
-} else {
-  msgCookies.classList.add('mostrar');
-}
-
-function aceito() {
-  localStorage.setItem('lgpd', 'sim');
-  msgCookies.classList.remove('mostrar');
-}
-
-// Coloque o botão para chamar essa função aceito() no seu HTML
-
-function aceito(){
-  localStorage
-}
